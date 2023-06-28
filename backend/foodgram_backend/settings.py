774fa97ececf46efc3f80238be56fce602875623
Каменский Николай <rambleroflife@yandex.ru>
2023-06-28 22:00:46 +0500

    Prefinal task v1.05

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -15,19 +15,19 @@ from pathlib import Path
 from datetime import timedelta
 import os
 
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-load_dotenv(dotenv_path=BASE_DIR.parent / '.env')
+# load_dotenv(dotenv_path=BASE_DIR.parent / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY', '')
+SECRET_KEY = os.getenv('SECRET_KEY', 'no_secret')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', False)
